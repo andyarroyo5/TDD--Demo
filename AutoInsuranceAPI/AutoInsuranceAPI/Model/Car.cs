19db4b4c23b360d9c68982575998e03e30c2a7f6
Andrea Arroyo <soarroyo@microsoft.com>
2020-03-30 10:55:23 -0600

    Create Solution + Models

---
+++ AutoInsuranceAPI/AutoInsuranceAPI/Model/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoInsuranceAPI.Model
+{
+    public class Car
+    {
+        public Int32 Year { get; set; }
+
+        public CarModels Model { get; set; }
+
+    }
+
+    public enum CarModels { 
+        
+        Vento,
+        Tida, 
+        Gol    
+    }
+
+   //Included as part of example 
+    public enum Location
+    {
+
+        Monterrey,
+        CDMX,
+        Guadalajara
+    }
+}
