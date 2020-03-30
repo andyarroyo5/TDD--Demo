f91b07b10cf8b34af883298b2c6992934294ec6d
Andrea Arroyo <soarroyo@microsoft.com>
2020-03-30 13:01:15 -0600

    Creating Our Test

--- AutoInsuranceAPI/AutoInsuranceTests/QuoteTests.cs
+++ AutoInsuranceAPI/AutoInsuranceTests/QuoteTests.cs
@@ -1,5 +1,6 @@
-using AutoInsuranceAPI.Controllers;
+using Moq;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace AutoInsuranceTests
@@ -7,16 +8,31 @@ namespace AutoInsuranceTests
     public class QuoteTests
     {
         private readonly QuoteController _quoteController;
+        private Mock<List<Quote>> _mockQuoteList;
 
         public QuoteTests()
         {
+            _mockQuoteList = new Mock<List<Quote>>();
             _quoteController = new QuoteController();
         }
 
         [Fact]
-        public void GetTest_GetQuote()
+        public void GetTest_GetQuotes()
         {
+            //fill 
+            var car = new Car {
+                Year=2016,
+                Model="Vento"
+            };
 
+            var location = "CDMX";
+            
+            //act
+            var result = _quoteController.Get();
+
+            //assert
+            var model = Assert.IsAssignableFrom<ActionResult<List<Quote>>(result);
+            Assert.Equal(3, model.Value.Count);
         }
     }
 }
